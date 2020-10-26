dc88e156e0197b25f72913e3e4f04c4d3c16aa6b
thanhminhsama <36581341+thanhminhsama@users.noreply.github.com>
2020-10-26 14:01:54 +0700

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -40,6 +40,7 @@ namespace animations {
      * @param e describe parameter here
      */
     //% block="blink x $x y $y every $interval ms"
+    //% interval.shadow=timePicker
     export function blink(x: MyEnum1, y: MyEnum1, interval: number): void {
         // Add code here
         let sprite = game.createSprite(x, y)
